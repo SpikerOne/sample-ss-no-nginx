da8185ea4605ef50c1e5ce5917a7fa945b3aa1aa
SpikerOne <96699983+SpikerOne@users.noreply.github.com>
2022-10-05 16:01:20 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,8 @@ rm -rf v2ray-plugin-linux-amd64-v1.3.2.tar.gz
 mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
 rm -rf /v2raybin
 
-cd /wwwroot
-tar xvf wwwroot.tar.gz
-rm -rf wwwroot.tar.gz
-
-mv /conf/nginx_ss.conf /etc/nginx/conf.d/ss.conf
 mv /conf/shadowsocks-libev_config.json /etc/shadowsocks-libev/config.json
 
 cat /etc/shadowsocks-libev/config.json
 
-ss-server -s 0.0.0.0 -p 443 -k password --plugin "v2ray-plugin" --plugin-opts "server;path=/v2ray" --no-delay --fast-open --reuse-port -u
+ss-server -s 0.0.0.0 -p 443 -k password --plugin "v2ray-plugin" --plugin-opts "server;path=/v2ray" -u
